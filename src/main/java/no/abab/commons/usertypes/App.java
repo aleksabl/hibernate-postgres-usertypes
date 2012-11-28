fa6e6b1dbcb550e9b837fbe1b7cddcf3025245b9
Aleksander Blomskøld <ablom@nets.eu>
2012-11-28 20:02:12 +0100

    Initialized maven project

---
+++ src/main/java/no/abab/commons/usertypes/App.java
@@ -0,0 +1,13 @@
+package no.abab.commons.usertypes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
